1d25ddaa815e2cfdf426c85efcaeeb9224103ee5
Masum-SMe <unusmasum0@gmail.comm>
2021-10-30 22:27:05 +0600

    upadate server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 const ObjectId = require('mongodb').ObjectId;
 
 // Middlewire 
@@ -12,6 +12,7 @@ app.use(express.json())
 
 
 const uri = ` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rhkgk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
@@ -39,13 +40,13 @@ async function run() {
         app.post('/services', async (req, res) => {
 
             const service = req.body
-            console.log('hit the post api', service)
+
 
 
 
 
             const result = await servicesCollection.insertOne(service)
-            console.log(result);
+
 
             res.json(result)
         });
@@ -86,7 +87,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello this is my')
 })
 
 app.listen(port, () => {
